02d4746000d40938b8463c7fb5de81c3970325fa
ismoildev007 <ismoil3436@gmail.com>
2024-08-07 14:22:42 +0500

    ismoil

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.page')
-WWWWass="row">
+@section('title', 'Blog Page')
+@section('content')
+    <!-- Breadcrumbs S t a r t -->
+    <section class="breadcrumb-section breadcrumb-bg">
+        <div class="container">
+            <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb-text">
                         <h1 class="title wow fadeInUp" data-wow-delay="0.1s">Blog</h1>
